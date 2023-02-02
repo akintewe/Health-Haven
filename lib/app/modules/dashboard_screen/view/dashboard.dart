09b23b9d1caefb93b5e273b096ab151108b6e7c8
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-02 11:01:48 +0100

    created dasboard loader

--- lib/app/modules/dashboard_screen/view/dashboard.dart
+++ lib/app/modules/dashboard_screen/view/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 import '../../../../core/utils/functions/size_fit.dart';
 
@@ -10,38 +11,8 @@ class DashboardScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-        body: Row(
-      children: [
-        SizedBox(
-          width: fitWidth(280),
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              vertical: fitHeight(35),
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        '',
-                        height: fitHeight(40),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
-        Expanded(
-          child: Container(
-            color: Colors.red,
-            child: child,
-          ),
-        )
-      ],
+        body: Center(
+      child: LottieBuilder.asset('assets/animation/medic.json'),
     ));
   }
 }
