3854ff5b5043a00e3b76d7e3ad88446e916bb102
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-06 19:41:10 +0100

    made the input pin page

--- lib/app/modules/forgot_password/views/input_pin.dart
+++ lib/app/modules/forgot_password/views/input_pin.dart
@@ -1,4 +1,5 @@
 import 'package:cancer_chat/app/modules/forgot_password/pin.dart';
+import 'package:cancer_chat/core/theme/colors.dart';
 import 'package:d_pin/d_pin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -16,10 +17,35 @@ class _PinInputState extends State<PinInput> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 90,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: AppColors.primary,
+            )),
+        title: Text(
+          'Forgot Password',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       body: Column(
         children: [
           SizedBox(
             height: 100,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [Text('Code has been sent to the selected address..')],
+          ),
+          SizedBox(
+            height: 30,
           ),
           DPin(
             buttonColor: Colors.blue,
