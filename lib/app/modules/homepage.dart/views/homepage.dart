25ed58164ea1d2fef473256e02a3644b26b51754
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-06 19:59:09 +0100

    creating home screen

--- lib/app/modules/homepage.dart/views/homepage.dart
+++ lib/app/modules/homepage.dart/views/homepage.dart
@@ -1,6 +1,8 @@
+import 'package:cancer_chat/core/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,6 +14,41 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 90,
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: Text(
+          'Doctor Q',
+          style: GoogleFonts.actor(
+              color: Colors.black, fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          Row(
+            children: [
+              Container(
+                height: 60,
+                width: 60,
+                decoration: BoxDecoration(
+                    color: AppColors.primary100,
+                    borderRadius: BorderRadius.circular(20)),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Container(
+                height: 60,
+                width: 60,
+                decoration: BoxDecoration(
+                    color: AppColors.primary100,
+                    borderRadius: BorderRadius.circular(20)),
+                  
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
