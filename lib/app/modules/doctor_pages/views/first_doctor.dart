4c3929b4087cd71fcb4d831c80a02e9b665c2c31
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-09 13:25:26 +0100

    creating doctor screens

---
+++ lib/app/modules/doctor_pages/views/first_doctor.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Doctor1 extends StatefulWidget {
+  const Doctor1({super.key});
+
+  @override
+  State<Doctor1> createState() => _Doctor1State();
+}
+
+class _Doctor1State extends State<Doctor1> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
