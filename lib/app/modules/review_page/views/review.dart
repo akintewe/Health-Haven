f25fcd6e88c256e80f086b23878231826c411124
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-09 17:43:50 +0100

    created review page

---
+++ lib/app/modules/review_page/views/review.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Reviews extends StatefulWidget {
+  const Reviews({super.key});
+
+  @override
+  State<Reviews> createState() => _ReviewsState();
+}
+
+class _ReviewsState extends State<Reviews> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
