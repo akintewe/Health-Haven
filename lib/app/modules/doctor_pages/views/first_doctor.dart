c010bb8cb5332d1eff02fffca57ac21d6155ebf3
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-09 13:38:50 +0100

    changes

--- lib/app/modules/doctor_pages/views/first_doctor.dart
+++ lib/app/modules/doctor_pages/views/first_doctor.dart
@@ -1,5 +1,8 @@
+import 'package:cancer_chat/core/theme/colors.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Doctor1 extends StatefulWidget {
   const Doctor1({super.key});
@@ -11,6 +14,22 @@ class Doctor1 extends StatefulWidget {
 class _Doctor1State extends State<Doctor1> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 90,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: AppColors.primary,
+              size: 27,
+            )),
+            title: Text('Dr. Jenny Wilson', style: GoogleFonts.actor(
+              color: Colors.black,
+            ),),
+      ),
+    );
   }
 }
