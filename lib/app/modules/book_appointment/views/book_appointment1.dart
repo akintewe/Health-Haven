7e004980c8ec16fd43c6b1673850c8b25323db57
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-17 19:37:06 +0100

    creating appointment page

--- lib/app/modules/book_appointment/views/book_appointment1.dart
+++ lib/app/modules/book_appointment/views/book_appointment1.dart
@@ -1,6 +1,8 @@
+import 'package:cancer_chat/core/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class BookAppointment1 extends StatefulWidget {
   const BookAppointment1({super.key});
@@ -12,6 +14,27 @@ class BookAppointment1 extends StatefulWidget {
 class _BookAppointment1State extends State<BookAppointment1> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: AppColors.primary,
+              size: 29,
+            )),
+        title: Text(
+          'Book Appointment',
+          style: GoogleFonts.roboto(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: ,
+    );
   }
 }
