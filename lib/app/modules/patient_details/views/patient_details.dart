cd117e8311afb01dcfc4d6198ad863e8cae7c864
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-18 23:08:00 +0100

    changes

--- lib/app/modules/patient_details/views/patient_details.dart
+++ lib/app/modules/patient_details/views/patient_details.dart
@@ -1,6 +1,8 @@
+import 'package:cancer_chat/core/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class PatientDetails extends StatefulWidget {
   const PatientDetails({super.key});
@@ -13,7 +15,25 @@ class _PatientDetailsState extends State<PatientDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.arrow_back_rounded,
+              color: AppColors.primary,
+              size: 27,
+            )),
+        title: Text(
+          'Patient Details',
+          style: GoogleFonts.roboto(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 30,
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/app/modules/patient_details/widgets/patient_detail_title_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PatientTitleWidget extends StatefulWidget {
+  const PatientTitleWidget({super.key});
+
+  @override
+  State<PatientTitleWidget> createState() => _PatientTitleWidgetState();
+}
+
+class _PatientTitleWidgetState extends State<PatientTitleWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Row();
+  }
+}
