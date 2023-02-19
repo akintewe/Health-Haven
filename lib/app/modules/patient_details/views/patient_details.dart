da8ea269ea96d7ffb87afe939c36b188e74dd564
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-19 22:48:44 +0100

    made changes to the screen

--- lib/app/modules/patient_details/views/patient_details.dart
+++ lib/app/modules/patient_details/views/patient_details.dart
@@ -22,6 +22,8 @@ class _PatientDetailsState extends State<PatientDetails> {
     'Prefer not to say',
   ];
   TextEditingController textEditingController = TextEditingController();
+  TextEditingController _notesController = TextEditingController();
+  TextEditingController phonenumberController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -124,7 +126,7 @@ class _PatientDetailsState extends State<PatientDetails> {
             Padding(
               padding: const EdgeInsets.only(left: 3, right: 8),
               child: TextFormField(
-                controller: textEditingController,
+                controller: phonenumberController,
                 decoration: InputDecoration(
                   isDense: true,
                   hintText: 'Phone Number',
@@ -145,32 +147,79 @@ class _PatientDetailsState extends State<PatientDetails> {
             SizedBox(
               height: 10,
             ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey,
-                  width: 1.0,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: DropdownButtonFormField(
-                  items: _gender.map((gender) {
-                    return DropdownMenuItem(
-                      value: gender,
-                      child: Text(gender),
-                    );
-                  }).toList(),
-                  decoration: InputDecoration(
-                    hintText: 'Gender',
-                    border: InputBorder.none,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
                   ),
-                  onChanged: (value) {
-                    setState(() {
-                      _selectedValue = value as String?;
-                    });
-                  }),
-            )
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: DropdownButtonFormField(
+                    items: _gender.map((gender) {
+                      return DropdownMenuItem(
+                        value: gender,
+                        child: Text(gender),
+                      );
+                    }).toList(),
+                    decoration: InputDecoration(
+                      hintText: 'Gender',
+                      border: InputBorder.none,
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        _selectedValue = value as String?;
+                      });
+                    }),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            PatientTitleWidget(title: 'Write your problem'),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                controller: _notesController,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                decoration: InputDecoration(
+                  hintText: 'Tell doctor about your problem...',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide(
+                      color: AppColors.grey10,
+                      width: 1.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              height: 60,
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: ElevatedButton(
+                onPressed: () {
+                  context.go('/patientDetails');
+                },
+                child: Text(
+                  'Next',
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50))),
+              ),
+            ),
           ],
         ),
       ),
