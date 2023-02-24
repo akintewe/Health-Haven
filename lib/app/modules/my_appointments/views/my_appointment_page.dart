a48ff25084a217d1f5ecfb34a721667bd7e80cb8
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-24 22:09:42 +0100

    creating my-appointment page

--- lib/app/modules/my_appointments/views/my_appointment_page.dart
+++ lib/app/modules/my_appointments/views/my_appointment_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../core/theme/colors.dart';
+import '../widgets/my_appointment_card_widget.dart';
 
 class MyAppointmentPage extends StatefulWidget {
   const MyAppointmentPage({super.key});
@@ -12,6 +17,64 @@ class MyAppointmentPage extends StatefulWidget {
 class _MyAppointmentPageState extends State<MyAppointmentPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {
+            context.go('/myAppointment');
+          },
+          icon: Icon(
+            Icons.arrow_back_rounded,
+            color: AppColors.primary,
+          ),
+        ),
+        title: Text(
+          'My Appointments',
+          style: GoogleFonts.roboto(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 23,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const AppointmentCard(
+                image: 'assets/images/doctor3.jpg',
+                name: 'DR. Mary Steward',
+                mode: 'Messages - ',
+                status: 'Completed',
+                time: '15:00 - 16:30PM',
+                icon1: Icons.message,
+                icon2: Icons.message,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  height: 150,
+                  width: MediaQuery.of(context).size.width * 0.92,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: AppColors.primary),
+                    borderRadius: BorderRadius.circular(20),
+                  ))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
