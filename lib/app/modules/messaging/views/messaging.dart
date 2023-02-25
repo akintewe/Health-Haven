584fddf4148a45a85cc5f216cfab15e9e9ab7be0
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2023-02-25 15:50:34 +0100

    changes

--- lib/app/modules/messaging/views/messaging.dart
+++ lib/app/modules/messaging/views/messaging.dart
@@ -1,4 +1,5 @@
 import 'package:cancer_chat/core/theme/colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -14,6 +15,8 @@ class MessagingPage extends StatefulWidget {
 }
 
 class _MessagingPageState extends State<MessagingPage> {
+  TextEditingController _controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,78 +63,238 @@ class _MessagingPageState extends State<MessagingPage> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Align(
-            alignment: Alignment.topCenter,
-            child: Container(
-              height: 100,
-              width: MediaQuery.of(context).size.width * 0.9,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  border: Border.all(
-                    color: AppColors.grey90,
-                  )),
-              child: Row(
-                children: [
-                  Stack(
-                    children: [
-                      Positioned(
-                        child: ClipRRect(
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            bottomLeft: Radius.circular(20),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: 100,
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    border: Border.all(
+                      color: AppColors.grey90,
+                    )),
+                child: Row(
+                  children: [
+                    Stack(
+                      children: [
+                        Positioned(
+                          child: ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              bottomLeft: Radius.circular(20),
+                            ),
+                            child: SizedBox(
+                                height: 100,
+                                width: 90,
+                                child: Image.asset(
+                                  'assets/images/doctor3.jpg',
+                                  fit: BoxFit.cover,
+                                )),
                           ),
-                          child: SizedBox(
-                              height: 100,
-                              width: 90,
-                              child: Image.asset(
-                                'assets/images/doctor3.jpg',
-                                fit: BoxFit.cover,
-                              )),
                         ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 13,
-                  ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Dr. Mary Steward',
-                        style: GoogleFonts.roboto(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16.sp,
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 13,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Dr. Mary Steward',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.sp,
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 10.sp,
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        '10:00 - 10:30AM',
-                        style: GoogleFonts.roboto(color: Colors.black),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    width: 18,
-                  ),
-                ],
+                        SizedBox(
+                          height: 10.sp,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          '10:00 - 10:30AM',
+                          style: GoogleFonts.roboto(color: Colors.black),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      width: 18,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Row(
-            children: [
-              Container(),
-            ],
-          ),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                      color: AppColors.grey100,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20))),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'Hello ${FirebaseAuth.instance.currentUser!.displayName!}, I am Dr Mary Steward.\nI will help you solve your disease\ncomplaints.',
+                            style: GoogleFonts.roboto(
+                                fontWeight: FontWeight.w400, fontSize: 14),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [Text('10:00')],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                      color: AppColors.grey100,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20))),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'First, can you tell me about your\nillness so far..',
+                            style: GoogleFonts.roboto(
+                                fontWeight: FontWeight.w400, fontSize: 14),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 60,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [Text('10:00')],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
+                SizedBox(
+                  width: 15,
+                ),
+                Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  decoration: BoxDecoration(
+                      color: AppColors.grey100,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(20),
+                          bottomLeft: Radius.circular(20),
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20))),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Column(
+                        children: [
+                          SizedBox(
+                            height: 15,
+                          ),
+                          Text(
+                            'This is very important so that i can\nhelp identify your disease and the\nsolution 😁',
+                            style: GoogleFonts.roboto(
+                                fontWeight: FontWeight.w400, fontSize: 14),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        width: 50,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [Text('10:00')],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: TextFormField(
+                controller: _controller,
+                decoration: InputDecoration(
+                  hintText: 'Type message....',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.send,
+                      color: AppColors.primary,
+                    ),
+                    onPressed: () {
+                      // handle send button press here
+                      print('Send button pressed!');
+                    },
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
